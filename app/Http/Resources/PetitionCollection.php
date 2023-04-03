64ff40e9bcaa74c3ed54c299980df9295d4ece3f
Filip Nedovic <Filip_Nedovic@epam.com>
2023-04-03 13:01:18 +0200

    petitions listing implemented

--- app/Http/Resources/PetitionCollection.php
+++ app/Http/Resources/PetitionCollection.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class PettitionCollection extends ResourceCollection
+class PetitionCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,10 @@ class PettitionCollection extends ResourceCollection
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'version' => '0.1.1',
+            'author' => 'Filip'
+        ];
     }
 }
