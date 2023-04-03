3fac99a564e9087180fe441d27c568b73ee89a03
Filip Nedovic <Filip_Nedovic@epam.com>
2023-04-03 12:48:59 +0200

    deleted unnecessary methods from Author controller

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -15,34 +15,11 @@ class AuthorController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
      */
     public function show(Author $author)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Author $author)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Author $author)
     {
         //
     }
